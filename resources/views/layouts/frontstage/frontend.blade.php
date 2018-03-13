8dc709db6f6b9d13ae5c2843c46ce84b7ed9043a
ren096358 <34854876+ren096358@users.noreply.github.com>
2018-03-13 14:16:36 +0800

    獨立 navbar 樣板

--- resources/views/layouts/frontstage/frontend.blade.php
+++ resources/views/layouts/frontstage/frontend.blade.php
@@ -9,6 +9,7 @@
         <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
             function hideURLbar(){ window.scrollTo(0,1); } </script>
         <!--//tags -->
+        <!--css -->
         <link href="{{asset('css/frontend/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all" />
         <link href="{{asset('css/frontend/style.css')}}" rel="stylesheet" type="text/css" media="all" />
         <link href="{{asset('css/frontend/font-awesome.css')}}" rel="stylesheet"> 
@@ -16,8 +17,26 @@
         <!-- //for bootstrap working -->
         <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet">
         <link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,900,900italic,700italic' rel='stylesheet' type='text/css'> 
+        @yield('css')
+        <!--//css -->
+        <!--js -->
+        
+        @yield('js')
+        <!--//js -->
     </head>
     <body>
+        <!-- footer -->
+        @include('layouts.frontstage.header')
+        <!-- //footer -->
+        
+        <!-- navbar -->
+        @include('layouts.frontstage.navbar')
+        <!-- //navbar -->
+        
         @yield('content')
+        
+        <!-- footer -->
+        @include('layouts.frontstage.footer')
+        <!-- //footer -->
     </body>
 </html>
