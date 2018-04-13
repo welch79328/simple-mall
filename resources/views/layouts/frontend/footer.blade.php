e369f854e7e89ede2eb0ab3bf31649d7074d26f1
ren096358 <34854876+ren096358@users.noreply.github.com>
2018-04-13 15:36:17 +0800

    * 新增 追蹤我們的連結

--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -25,8 +25,8 @@
         <div class="col-sm-9 col-sm-offset-3  col-md-8 col-md-offset-4">
             <h3 class="footer_title">追蹤我們</h3>
             <div class="center-block text-left footer_detail">
-                <div><a href="">Facebook</a></div>
-                <div><a href="">異業合作</a></div>
+                <div><a href="https://www.facebook.com/JUGOmall/">Facebook</a></div>
+                <div><a href="https://www.facebook.com/freeadwifistores/">異業合作</a></div>
             </div>
         </div>
         {{--<div class="col-sm-12 col-md-offset-2 col-md-8 " hidden>--}}
@@ -43,111 +43,3 @@
         {{--</div>--}}
     </div>
 </div>
-<!-- //footer -->
-{{--<!--<div class="footer">--}}
-{{--<div class="footer_agile_inner_info_w3l">--}}
-{{--<div class="col-md-3 footer-left">--}}
-{{--<h2><a href="{{url('')}}"><span>E</span>lite Shoppy </a></h2>--}}
-{{--<p>Lorem ipsum quia dolor--}}
-{{--sit amet, consectetur, adipisci velit, sed quia non--}}
-{{--numquam eius modi tempora.</p>--}}
-{{--<ul class="social-nav model-3d-0 footer-social w3_agile_social two">--}}
-{{--<li><a href="#" class="facebook">--}}
-{{--<div class="front"><i class="fa fa-facebook" aria-hidden="true"></i></div>--}}
-{{--<div class="back"><i class="fa fa-facebook" aria-hidden="true"></i></div></a></li>--}}
-{{--<li><a href="#" class="twitter">--}}
-{{--<div class="front"><i class="fa fa-twitter" aria-hidden="true"></i></div>--}}
-{{--<div class="back"><i class="fa fa-twitter" aria-hidden="true"></i></div></a></li>--}}
-{{--<li><a href="#" class="instagram">--}}
-{{--<div class="front"><i class="fa fa-instagram" aria-hidden="true"></i></div>--}}
-{{--<div class="back"><i class="fa fa-instagram" aria-hidden="true"></i></div></a></li>--}}
-{{--<li><a href="#" class="pinterest">--}}
-{{--<div class="front"><i class="fa fa-linkedin" aria-hidden="true"></i></div>--}}
-{{--<div class="back"><i class="fa fa-linkedin" aria-hidden="true"></i></div></a></li>--}}
-{{--</ul>--}}
-{{--</div>--}}
-{{--<div class="col-md-9 footer-right">--}}
-{{--<div class="sign-grds">--}}
-{{--<div class="col-md-4 sign-gd">--}}
-{{--<h4>Our <span>Information</span> </h4>--}}
-{{--<ul>--}}
-{{--<li><a href="{{url('/')}}">Home</a></li>--}}
-{{--<li><a href="{{url('mens')}}">Men's Wear</a></li>--}}
-{{--<li><a href="{{url('womens')}}">Women's wear</a></li>--}}
-{{--<li><a href="{{url('about')}}">About</a></li>--}}
-{{--<li><a href="{{url('typography')}}">Short Codes</a></li>--}}
-{{--<li><a href="{{url('contact')}}">Contact</a></li>--}}
-{{--</ul>--}}
-{{--</div>--}}
-
-{{--<div class="col-md-5 sign-gd-two">--}}
-{{--<h4>Store <span>Information</span></h4>--}}
-{{--<div class="w3-address">--}}
-{{--<div class="w3-address-grid">--}}
-{{--<div class="w3-address-left">--}}
-{{--<i class="fa fa-phone" aria-hidden="true"></i>--}}
-{{--</div>--}}
-{{--<div class="w3-address-right">--}}
-{{--<h6>Phone Number</h6>--}}
-{{--<p>+1 234 567 8901</p>--}}
-{{--</div>--}}
-{{--<div class="clearfix"> </div>--}}
-{{--</div>--}}
-{{--<div class="w3-address-grid">--}}
-{{--<div class="w3-address-left">--}}
-{{--<i class="fa fa-envelope" aria-hidden="true"></i>--}}
-{{--</div>--}}
-{{--<div class="w3-address-right">--}}
-{{--<h6>Email Address</h6>--}}
-{{--<p>Email :<a href="mailto:example@email.com"> mail@example.com</a></p>--}}
-{{--</div>--}}
-{{--<div class="clearfix"> </div>--}}
-{{--</div>--}}
-{{--<div class="w3-address-grid">--}}
-{{--<div class="w3-address-left">--}}
-{{--<i class="fa fa-map-marker" aria-hidden="true"></i>--}}
-{{--</div>--}}
-{{--<div class="w3-address-right">--}}
-{{--<h6>Location</h6>--}}
-{{--<p>Broome St, NY 10002,California, USA.--}}
-
-{{--</p>--}}
-{{--</div>--}}
-{{--<div class="clearfix"> </div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--<div class="col-md-3 sign-gd flickr-post">--}}
-{{--<h4>Flickr <span>Posts</span></h4>--}}
-{{--<ul>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t1.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t2.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t3.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t4.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t1.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t2.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t3.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t2.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--<li><a href="{{url('single')}}"><img src="/images/t4.jpg" alt=" " class="img-responsive" /></a></li>--}}
-{{--</ul>--}}
-{{--</div>--}}
-{{--<div class="clearfix"></div>--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--<div class="clearfix"></div>--}}
-{{--<div class="agile_newsletter_footer">--}}
-{{--<div class="col-sm-6 newsleft">--}}
-{{--<h3>SIGN UP FOR NEWSLETTER !</h3>--}}
-{{--</div>--}}
-{{--<div class="col-sm-6 newsright">--}}
-{{--<form action="#" method="post">--}}
-{{--<input type="email" placeholder="Enter your email..." name="email" required="">--}}
-{{--<input type="submit" value="Submit">--}}
-{{--</form>--}}
-{{--</div>--}}
-
-{{--<div class="clearfix"></div>--}}
-{{--</div>--}}
-{{--<p class="copy-right">&copy 2017 Elite shoppy. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>--}}
-{{--</div>--}}
-{{--</div>-->--}}
