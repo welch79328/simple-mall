91adfd27ce8e68aa59e9096065037cc0f190418b
ren096358 <34854876+ren096358@users.noreply.github.com>
2018-04-23 21:55:38 +0800

    * 修正 bug

--- resources/views/backstage/returns/index.blade.php
+++ resources/views/backstage/returns/index.blade.php
@@ -24,7 +24,7 @@
             <div class="result_content">
                 <div class="short_wrap">
                     {{--<a href="{{url('admin/order/create')}}"><i class="fa fa-plus"></i>添加退貨單</a>--}}
-                    <a href="{{url('admin/return')}}"><i class="fa fa-recycle"></i>全部退貨單</a>
+                    <a href="{{url('admin/returns')}}"><i class="fa fa-recycle"></i>全部退貨單</a>
                 </div>
             </div>
             <!--快捷导航 结束-->
@@ -44,13 +44,15 @@
                     @foreach($data as $v)
                         <tr>
                             <td><a href="{{url('admin/order/'.$v->order_id)}}">{{$v->order_number}}</a></td>
-                            <td style="color: @if($v->return_status == 'complete') #009966 @endif">{{$v->_returns_status}}</td>
+                            <td style="color: @if($v->returns_status == 'complete') #009966 @endif">{{$v->_returns_status}}</td>
                             <td>{{$v->editor}}</td>
                             <td>{{$v->created_at}}</td>
                             <td>{{$v->updated_at}}</td>
                             <td>
                                 <a href="{{url('admin/returns_edit/'.$v->returns_id)}}">查看</a>
-                                <a href="{{url('admin/returns_complete/'.$v->returns_id)}}">已解決</a>
+                                @if($v->returns_status != 'complete')
+                                    <a href="{{url('admin/returns_complete/'.$v->returns_id)}}">已解決</a>
+                                @endif
                             </td>
                         </tr>
                     @endforeach
